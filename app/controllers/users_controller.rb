eb75c8bf6fdcfe70dfec676248cc5562720f6821
ammar khan <khan.ammar6k@gmail.com>
2025-04-21 16:57:59 +0530

    added users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,8 +28,12 @@ class UsersController < ApplicationController
     else
       render "edit"
     end
-
-
   end
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
 
 end
