6b34878afc062f5fc7474e5cbef51edba14b301f
ammar khan <khan.ammar6k@gmail.com>
2025-04-21 13:20:56 +0530

    added new user signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      flash[:notice] = "Welcome  #{@user.username} to the Alpha Blog."
+      redirect_to articles_path
+    else
+      render "new"
+    end
   end
 
 end
